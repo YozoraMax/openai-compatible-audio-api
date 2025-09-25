5c769bfe9cac09ed7a3b3d8281604c64ef9e70fd
bo <bo@nj-bo-yu-m.client.tw.trendnet.org>
2025-09-25 13:34:27 +0800

    fix

--- Dolphin/dolphin/__init__.py
+++
@@ -1,5 +0,0 @@
-# encoding: utf8
-
-from .audio import load_audio
-from .transcribe import load_model, transcribe
-from .version import __version__
--- Dolphin/dolphin/__main__.py
+++
@@ -1,5 +0,0 @@
-# encoding: utf8
-
-from .transcribe import cli
-
-cli()
